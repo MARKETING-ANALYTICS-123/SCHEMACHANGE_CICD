d41c4c6dc4ea4d913d5a9daf7708686eafdff9b7
MARKETING-ANALYTICS-123 <ankitmarketing33@gmail.com>
2025-05-29 17:11:05 +0530

    Update and rename Tasks_Child.sql to Tasks_Child_03.sql

--- dbscripts2/MARKETING/XFRM/Tasks/Tasks_Child_03.sql
+++ dbscripts2/MARKETING/XFRM/Tasks/Tasks_Child_03.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE TASK task_child
+CREATE OR REPLACE TASK task_child_03
   WAREHOUSE = COMPUTE_WH
-  AFTER task_mid
+  AFTER task_mid_02
 AS
   SELECT 'This is the child tak.' AS msg;
