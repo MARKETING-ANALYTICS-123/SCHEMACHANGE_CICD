e89e8557b45b1d4d9b94e2a92ff45a61135142d0
MARKETING-ANALYTICS-123 <ankitmarketing33@gmail.com>
2025-05-29 18:25:56 +0530

    Delete dbscripts2/MARKETING/XFRM/Tasks/TASK_ROOT.sql

--- dbscripts2/MARKETING/XFRM/Tasks/TASK_ROOT.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE TASK TASK_ROOT
-  WAREHOUSE = COMPUTE_WH
-  SCHEDULE = 'USING CRON 0 * * * * UTC'
-AS
-  -- Root task: simple insert or refresh statement
-  INSERT INTO XFRM.MY_TABLE_ROOT (id, val) VALUES (1, 'root');
