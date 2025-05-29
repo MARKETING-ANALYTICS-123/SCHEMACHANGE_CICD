8bdffe865eae3f66ce6fc94350be806d51dea614
MARKETING-ANALYTICS-123 <ankitmarketing33@gmail.com>
2025-05-29 18:29:22 +0530

    Update TASK_ROOT.sql

--- dbscripts2/MARKETING/XFRM/Tasks/TASK_ROOT.sql
+++ dbscripts2/MARKETING/XFRM/Tasks/TASK_ROOT.sql
@@ -3,4 +3,4 @@ CREATE OR REPLACE TASK TASK_ROOT
   SCHEDULE = 'USING CRON 0 * * * * UTC'
 AS
   -- Root task: simple insert or refresh statement
-  INSERT INTO XFRM.MY_TABLE_ROOT (id, val) VALUES (1, 'root');
+  CREATE OR REPLACE TABLE T1 AS SELECT 1 AS A;
